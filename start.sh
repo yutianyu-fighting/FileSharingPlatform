67dc57d66424ce4cf55868830deab612f7315e2c
yutianyu-fighing <1335592971@qq.com>
2020-06-17 16:46:34 +0800

    firstVersion

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo
+echo ============= fastdfs ==============
+# 关闭已启动的 tracker 和 storage
+#./fastdfs.sh stop
+# 启动 tracker 和 storage
+#./fastdfs.sh all
+sudo fdfs_trackerd  /etc/fdfs/tracker.conf
+sudo fdfs_storaged  /etc/fdfs/storage.conf
+# 重启所有的 cgi程序
+echo
+echo ============= fastCGI ==============
+./fcgi.sh
+# 关闭nginx
+echo
+echo ============= nginx ==============
+./nginx.sh stop
+# 启动nginx
+./nginx.sh start
+# 关闭redis
+echo
+echo ============= redis ==============
+./redis.sh stop
+# 启动redis
+./redis.sh start
